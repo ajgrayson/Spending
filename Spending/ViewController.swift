50b90428c3d0536d90b88b50112ba082834268a4
John Grayson <ajgrayson@gmail.com>
2015-08-05 10:05:55 +1200

    Initial Commit

---
+++ Spending/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Spending
+//
+//  Created by John Grayson on 5/08/15.
+//  Copyright © 2015 John Grayson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SpendingUITests/SpendingUITests.swift
@@ -0,0 +1,34 @@
+//
+//  SpendingUITests.swift
+//  SpendingUITests
+//
+//  Created by John Grayson on 5/08/15.
+//  Copyright © 2015 John Grayson. All rights reserved.
+//
+
+import XCTest
+
+class SpendingUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
